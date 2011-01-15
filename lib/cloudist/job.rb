4635d7ae8639de2467c2db70399220c4afdbcabc
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-01-15 19:30:54 +1100

    Added basic reply, and event handling to job

--- lib/cloudist/job.rb
+++ lib/cloudist/job.rb
@@ -18,7 +18,39 @@ module Cloudist
     end
     
     def cleanup
-      log.info("Cleanup")
+      
+    end
+    
+    def reply(data, headers = {})
+      # opts.merge!(default_publish_opts)
+      # reply_to = droid_headers[:reply_to] || self.msg['reply_to']
+      # raise UnknownReplyTo unless reply_to
+      # JobQueue.publish_to_q(reply_to, data, opts, popts)
+      
+      # Factory.log.debug("Reply queue: #{request.reply_to}")
+      # 
+      # reply_queue = ReplyQueue.new(request.reply_to)
+      # reply_queue.setup
+      # 
+      # response, headers = Factory::Utils.format_publish(data, headers)
+      # reply_queue.q.publish(response, headers)
+      log.debug("Replying: #{data.inspect} - Headers: #{headers.inspect}")
+      
+      
+      
+    end
+    
+    def event(event_name, data = {})
+      data = {} unless data
+      reply({:event => event_name})
+    end
+    
+    def method_missing(meth, *args, &blk)
+      if meth.to_s.ends_with?("!")
+        event(meth.to_s.gsub(/(!)$/, ''), args.shift)
+      else
+        super
+      end
     end
     
   end
