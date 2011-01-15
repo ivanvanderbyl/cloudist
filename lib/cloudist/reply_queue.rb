ff0f4a973a1d39bba5b0534680983ca54d1590fb
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-01-16 09:36:12 +1100

    Started writing ReplyQueue spec

--- lib/cloudist/reply_queue.rb
+++ lib/cloudist/reply_queue.rb
@@ -1,6 +1,32 @@
 module Cloudist
   class ReplyQueue < Cloudist::BasicQueue
-    
+    def initialize(queue_name, opts={})
+      opts[:auto_delete] = true
+      super
+    end
+
+    def setup(key = nil)
+      @mq = MQ.new
+      @q = @mq.queue(queue_name, opts)
+      @ex = @mq.direct
+      if key
+        @q.bind(@ex, :key => key)
+      else
+        @q.bind(@ex)
+      end
+    end
+
+    # def subscribe(amqp_opts={}, opts={})
+    #   super(amqp_opts, opts) do |request|
+    #     yield request if block_given?
+    #     self.destroy
+    #   end
+    # end
+    # 
+    # def teardown
+    #   @q.delete
+    #   super
+    # end
     
   end
 end
