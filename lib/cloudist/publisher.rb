3d4b540cb0a9b32752e72a296cccdfd55e4b4363
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-01-15 17:39:06 +1100

    Updated Publisher to reflect new publish methods on BasicQueue

--- lib/cloudist/publisher.rb
+++ lib/cloudist/publisher.rb
@@ -2,7 +2,15 @@ module Cloudist
   class Publisher
     
     class << self
-      
+      def enqueue(queue_name, data)
+        payload = Cloudist::Payload.new(data)
+
+        queue = Cloudist::JobQueue.new(queue_name)
+        queue.setup
+        queue.publish(payload)
+        
+        return Job.new(payload)
+      end
     end
     
   end
