133c219afa77e2d34287231e2a46c8b27b4f1677
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-01-16 13:02:48 +1100

    Removed config

--- examples/queue_message.rb
+++ examples/queue_message.rb
@@ -2,8 +2,6 @@ $:.unshift File.dirname(__FILE__) + '/../lib'
 require "rubygems"
 require "cloudist"
 
-ENV["AMQP_URL"] = 'amqp://test_pilot:t35t_p1l0t!@ec2-50-16-134-211.compute-1.amazonaws.com:5672/'
-
 ::Signal.trap('INT') { Cloudist.stop }
 ::Signal.trap('TERM'){ Cloudist.stop }
 
