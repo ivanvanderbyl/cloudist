b2450842e55a6aae60faa0f2527450999a706bb8
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-01-20 11:48:07 +1100

    Added example using Class instead of block for worker

---
+++ examples/sandwich_worker_with_class.rb
@@ -0,0 +1,37 @@
+# Cloudst Example: Sandwich Worker
+# 
+# This example demonstrates receiving a job and sending back events to the client to let it know we've started and finsihed
+# making a sandwich. From here you could dispatch an eat.sandwich event.
+# 
+# Be sure to update the Cloudist connection settings if they differ from defaults:
+# user: guest
+# pass: guest
+# port: 5672
+# host: localhost
+# vhost: /
+# 
+$:.unshift File.dirname(__FILE__) + '/../lib'
+require "rubygems"
+require "cloudist"
+
+class SandwichWorker < Cloudist::Worker
+  def process
+    log.info("Processing queue: #{queue.name}")
+    log.info(data.inspect)
+    
+    job.started!
+    (1..20).each do |i|
+      job.progress(i * 5)
+      sleep(1)
+      
+      raise ArgumentError, "NOT GOOD!" if i == 4
+    end
+    job.finished!
+  end
+end
+
+Cloudist.signal_trap!
+
+Cloudist.start {
+  Cloudist.handle('make.sandwich', 'eat.sandwich').with(SandwichWorker)
+}
