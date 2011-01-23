0b25a05cd6a71d75712c3ee871fb8cd9f247b015
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-01-23 15:17:02 +1100

    Added custom error callback

--- lib/cloudist/callbacks/error_callback.rb
+++ lib/cloudist/callbacks/error_callback.rb
@@ -1,14 +1,40 @@
 module Cloudist
-  class ErrorCallback < Callback  
+  class ErrorCallback < Callback
     def call(payload)
       @payload = payload
       
       case source.arity
       when 1
-        instance_exec(payload.exception, &source)
+        instance_exec(SafeError.new(payload), &source)
       else
         instance_exec(&source)
       end
+    end
+  end
+  
+  class SafeError
+    def initialize(payload)
+      @payload = payload
+    end
+    
+    def message
+      @payload.message
+    end
+    
+    def class_name
+      @payload.exception
+    end
+    
+    def name
+      class_name
+    end
+    
+    def to_s
+      message
+    end
+    
+    def backtrace
+      @payload.backtrace
     end
   end
 end
