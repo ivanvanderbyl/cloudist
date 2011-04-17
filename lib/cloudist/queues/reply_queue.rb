bb07b81449fa3ca62e1bbfc6740b1cc6694c5ed0
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-04-17 22:40:31 +1000

    Updated basic Reply Queue

--- lib/cloudist/queues/reply_queue.rb
+++ lib/cloudist/queues/reply_queue.rb
@@ -1,41 +1,23 @@
 module Cloudist
-  class ReplyQueue < Cloudist::Queues::BasicQueue
-    def initialize(queue_name, opts={})
-      raise ArgumentError, "You must supply a queue_name" unless queue_name
-      queue_name = Utils.reply_prefix(queue_name) unless queue_name.starts_with?(Utils.reply_prefix(''))
-      opts[:auto_delete] = true
-      opts[:nowait] = true
-      super
+  class ReplyQueue < Cloudist::Queue
+    def initialize(name, options = {})
+      name = Utils.reply_prefix(name) unless name.starts_with?(Utils.reply_prefix(''))
+      options[:auto_delete] = true
+      options[:nowait] = true
+      super(name, options)
     end
-
-    # def setup(key = nil)
-    #       @mq = AMQP::Channel.new
-    #       @mq.prefetch(1)
-    #       @q = @mq.queue(queue_name, opts)
-    #       @ex = @mq.direct
-    #       if key
-    #         @q.bind(@ex, :key => key)
-    #       else
-    #         @q.bind(@ex)
-    #       end
-    #     end
     
-    # def setup_exchange
-    #   @ex = channel.direct
-    #   q.bind(ex)
-    # end
-
-    # def subscribe(amqp_opts={}, opts={})
-    #   super(amqp_opts, opts) do |request|
-    #     yield request if block_given?
-    #     self.destroy
-    #   end
-    # end
-    # 
-    # def teardown
-    #   @q.delete
-    #   super
-    # end
+    def publish(msg)
+      raise ArgumentError, "Publish expects a Cloudist::Message object" unless msg.is_a?(Cloudist::Message)
+      
+      body, headers = msg.encoded
+      publish_to_q(body, headers)
+      p msg.body.to_hash
+    end
     
+    def setup_exchange
+      @ex = channel.direct
+      setup_binding
+    end
   end
 end
