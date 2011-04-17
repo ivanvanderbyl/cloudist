2a05a3d27c7a2728208bbd747fca36331db8ed2e
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-04-17 22:42:54 +1000

    Imported global methods from old cloudist

--- lib/cloudist.rb
+++ lib/cloudist.rb
@@ -5,9 +5,11 @@ require "logger"
 require "digest/md5"
 require "uuid"
 require "active_support/core_ext/module/delegation"
+require "hashie"
 
 $:.unshift File.dirname(__FILE__)
 
+require "em/em_timer_utils"
 require "cloudist/core_ext/string"
 require "cloudist/core_ext/object"
 require "cloudist/core_ext/class"
@@ -18,10 +20,13 @@ module Cloudist
   DEFAULT_TTL = 300
   
   autoload :Utils, "cloudist/utils"
+  autoload :Encoding, "cloudist/encoding"
   autoload :Message, 'cloudist/message'
   autoload :Queue, 'cloudist/queue'
+  autoload :ReplyQueue, 'cloudist/queues/reply_queue'
   autoload :Application, 'cloudist/application'
-  autoload :Hashie, 'hashie'
+  autoload :Request, 'cloudist/request'
+  autoload :Messaging, 'cloudist/messaging'
   
   class << self
     # delegate :start, :to => Application
@@ -33,6 +38,32 @@ module Cloudist
     def log=(log)
       @@log = log
     end
+    
+    def closing?
+      ::AMQP.closing?
+    end
+    
+    # Call this at anytime inside the loop to exit the app.
+    def stop_safely
+      if EM.reactor_running?
+        ::EM.add_timer(0.2) { 
+          ::AMQP.stop { 
+            ::EM.stop
+            puts "\n"
+          }
+        }
+      end
+    end
+    
+    alias :stop :stop_safely
+    
+    def handle_error(e)
+      log.error "#{e.class}: #{e.message}"#, :exception => e
+      e.backtrace.each do |line|
+        log.error line
+      end
+    end
   end
   
+  include Cloudist::EMTimerUtils
 end
