c6ae302482526e8360f3954009e9d4c4afe7c2ab
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-05-24 14:01:51 +1000

    Changed encoding to JSON so we don't end up with with classes which can't be constantized on the other end

--- lib/cloudist/encoding.rb
+++ lib/cloudist/encoding.rb
@@ -1,13 +1,16 @@
 module Cloudist
   module Encoding
     def encode(message)
-      Marshal.dump(message)
+      # Marshal.dump(message)
+      # JSON.dump(message.to_hash)
+      message.to_json
     end
 
     def decode(message)
       raise ArgumentError, "First argument can't be nil" if message.nil?
       return message unless message.is_a?(String)
-      Marshal.load(message)
+      # Marshal.load(message)
+      JSON.load(message)
     end
   end
 end
